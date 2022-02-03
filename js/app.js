8752b174bd6b13e61163a950b695630c6f5d18bb
770eitan <carl158@gmail.com>
2022-02-03 15:20:43 -0500

    finish

--- js/app.js
+++ js/app.js
@@ -2,20 +2,30 @@ let h1Element = document.querySelector('h1')
 const inputElement = document.getElementById('Input')
 const btn = document.getElementById('submit-button')
 const ul = document.getElementById('todo-list')
-const resetBtn =document.getElementById('reset-button')
+const resetBtn = document.getElementById('reset-button')
 
-btn.addEventListener('click',function(evt){
+btn.addEventListener('click', function (evt) {
     const newLi = document.createElement('li')
     const inp = document.querySelector('Input')
     newLi.textContent = inp.value
-  if (inp.value !== "") {  document.querySelector('ul').appendChild(newLi)
-  inp.value = null
-}
+    if (inp.value !== "") {
+        document.querySelector('ul').appendChild(newLi)
+        inp.value = null
+    }
+    //delete button on click
 
-resetBtn.addEventListener('click',function(evt){
-    newLi.textContent=' '
+//ADD a Reset button
+    resetBtn.addEventListener('click', function (evt) {
+        newLi.textContent = ' '
+    })
 })
-})  
+document.querySelector('ul').addEventListener('click', handleClick)
+function handleClick(evt) {
+
+  console.log(evt.target)
+  const liToDelete = evt.target
+  liToDelete.textContent = ''
+}
 
 
 console.log(resetBtn) 
